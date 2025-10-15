74c93c7fb714a8f3dd8725ae1cb300e7d3bc9331
Shalev Widen <shalevwiden@gmail.com>
2025-10-14 23:11:49 -0500

    adding more JavaScript

--- web_stuff/static/js/toggleanimations.js
+++ web_stuff/static/js/toggleanimations.js
@@ -4,9 +4,24 @@ const blurbifybutton = buttonsDiv.querySelector("#blurbifybutton");
 const Backgroundbutton = buttonsDiv.querySelector("#Backgroundbutton");
 const flyingbutton = buttonsDiv.querySelector("#flyingbutton");
 
-const blurbifydiv=document.querySelector(".blurbifydiv")
+const blurbifydiv = document.querySelector(".blurbifydiv");
 
-if blurbifybutton toggled{
-blurbifydiv.classList.toggle('blurbifyanimations')
+const body = document.querySelector("body");
 
-}
+const flyingimage = document.querySelector("img.flyingimage");
+
+// now the toggling logic here
+blurbifybutton.addEventListener("click", () => {
+  console.log("toggling blurbify");
+  blurbifydiv.classList.toggle("blurbifyanimations");
+});
+
+Backgroundbutton.addEventListener("click", () => {
+  console.log("toggling background");
+  body.classList.toggle("backgroundanimations");
+});
+
+flyingbutton.addEventListener("click", () => {
+  console.log("toggling flying object");
+  flyingimage.classList.toggle("flyinganimations");
+});
