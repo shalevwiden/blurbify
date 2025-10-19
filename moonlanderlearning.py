36345a3a7a3b1c95c57c273aacd86e75ee690415
Shalev Widen <shalevwiden@gmail.com>
2025-10-19 15:41:36 -0500

    Updated ffmpeg rendervid.py code

--- moonlanderlearning.py
+++
@@ -1,5 +0,0 @@
-hm!#%%^cdsdsadasfasdfwasdasd#$(#$(#$(#$())))
-jkjk`^`xcxcczzxcvbbbvcxzzzxcvbbbvcxzzzxcvbbbvcnmv,mxcnv,mznxc,vmnzc,vhhh4789123456789
-(){}{}()(){}()
-
-
--- rendervid.py
+++ rendervid.py
@@ -4,9 +4,17 @@
 import subprocess
 import os
 
-imagespath='/Users/shalevwiden/Downloads/Projects/blurbifyoutput/demovid'
+imagespath='/Users/shalevwiden/Downloads/Projects/blurbify/outputs/demovid'
 
-donepath='/Users/shalevwiden/Downloads/Projects/blurbifyoutput/vidoutput'
+donepath='/Users/shalevwiden/Downloads/Projects/blurbify/outputs/vidoutput'
+
+def checkpaths():
+    if os.path.exists(imagespath):
+        print('first good')
+    if os.path.exists(donepath):
+        print('second good')
+    else:
+        print(f'not found\n\n\n\n')
 
 framerate = 10                 # frames per second
 output_file =os.path.join(donepath,'donevid.mp4')      # output file
@@ -34,22 +42,16 @@ makefilelist()
 def createvid():
     cmd = [
         "ffmpeg",
-        # tell FFmpeg it's a file list
-        "-pattern_type", "glob",
-              
-        "-safe", "0",                
-        "-i", file_list_path,
-            "-vf", "scale=1920:1080:force_original_aspect_ratio=decrease,pad=1920:1080:(ow-iw)/2:(oh-ih)/2",
-
-            "-r", str(framerate),       # output framerate
-
+        "-f", "concat",        # tell FFmpeg it's a list of files
+        "-safe", "0",          # allow absolute paths
+        "-i", file_list_path,  # path to file_list.txt
+        "-vf", "scale=1920:1080:force_original_aspect_ratio=decrease,pad=1920:1080:(ow-iw)/2:(oh-ih)/2",
+        "-r", str(framerate),
         "-c:v", codec,
         "-pix_fmt", pix_fmt,
         output_file
     ]
 
-    # === Run command ===
     subprocess.run(cmd, check=True)
-    print('created vid')
-
+    print("✅ Created video successfully!")
 createvid()
